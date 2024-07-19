c54633bf44dd4acce6be20ac9fdc934ee3dece7a
Anuj Sinha <sinha.anuj30@gmail.com>
2024-07-19 15:00:13 -0700

    test: use tmp_path fixture

--- tests/workflows/test_workflow1.py
+++ tests/workflows/test_workflow1.py
@@ -11,46 +11,46 @@ from mawpy.steps.update_stay_duration import update_stay_duration
 from mawpy.workflows.workflow1 import workflow1
 
 
-def test_workflow1_ic_usd():
-    with TemporaryDirectory() as tempdir:
-        input_file = os.path.dirname(__file__) + '/../resources/test_input.csv'
-        output_file = os.path.join(tempdir, 'test_output_workflow1.csv')
+def test_workflow1_ic_usd(tmp_path):
 
-        # Prepare arguments similar to command-line arguments
-        args = Namespace(
-            input_file=input_file,
-            output_file=output_file,
-            spatial_constraint=1,
-            duration_constraint_1=0,
-            duration_constraint_2=300
-        )
+    input_file = os.path.dirname(__file__) + '/../resources/test_input.csv'
+    output_file = os.path.join(tmp_path, 'test_output_workflow2.csv')
 
-        # Run the workflow to be tested and get the output
-        actual_output_df = workflow1(args.input_file, args.output_file, args.spatial_constraint,
-                  args.duration_constraint_1, args.duration_constraint_2)
+    # Prepare arguments similar to command-line arguments
+    args = Namespace(
+        input_file=input_file,
+        output_file=output_file,
+        spatial_constraint=1,
+        duration_constraint_1=0,
+        duration_constraint_2=300
+    )
 
-        # Check if output file was created
-        assert os.path.exists(output_file)
+    # Run the workflow to be tested and get the output
+    actual_output_df = workflow1(args.input_file, args.output_file, args.spatial_constraint,
+              args.duration_constraint_1, args.duration_constraint_2)
 
-        # Destroy test output file after test completed
-        os.remove(args.output_file)
+    # Check if output file was created
+    assert os.path.exists(output_file)
 
-        # Check if the workflow steps were called with expected arguments
-        df_output = incremental_clustering(output_file, args.spatial_constraint, args.duration_constraint_1,
-                                           input_file=args.input_file)
-        expected_output_df = update_stay_duration(output_file, args.duration_constraint_2, input_df=df_output)
+    # Destroy test output file after test completed
+    os.remove(args.output_file)
 
-        # Assert if the expected_output_df equal to actual_output_df
-        pd.testing.assert_frame_equal(actual_output_df, expected_output_df)
+    # Check if the workflow steps were called with expected arguments
+    df_output = incremental_clustering(output_file, args.spatial_constraint, args.duration_constraint_1,
+                                       input_file=args.input_file)
+    expected_output_df = update_stay_duration(output_file, args.duration_constraint_2, input_df=df_output)
 
-        # List of columns expected in the actual_output_df
-        expected_output_columns = [USER_ID, UNIX_START_T, UNIX_START_DATE,
-                                   ORIG_LAT, ORIG_LONG, ORIG_UNC,
-                                   STAY_LAT, STAY_LONG, STAY_UNC,
-                                   STAY, STAY_DUR]
+    # Assert if the expected_output_df equal to actual_output_df
+    pd.testing.assert_frame_equal(actual_output_df, expected_output_df)
 
-        # Check if all the expected columns are present in the workflow output columns
-        assert len(list(set(expected_output_columns) & set(actual_output_df.columns))) == len(expected_output_columns)
+    # List of columns expected in the actual_output_df
+    expected_output_columns = [USER_ID, UNIX_START_T, UNIX_START_DATE,
+                               ORIG_LAT, ORIG_LONG, ORIG_UNC,
+                               STAY_LAT, STAY_LONG, STAY_UNC,
+                               STAY, STAY_DUR]
 
-        # Check if output file was written successfully
-        assert os.path.exists(args.output_file)
+    # Check if all the expected columns are present in the workflow output columns
+    assert len(list(set(expected_output_columns) & set(actual_output_df.columns))) == len(expected_output_columns)
+
+    # Check if output file was written successfully
+    assert os.path.exists(args.output_file)
