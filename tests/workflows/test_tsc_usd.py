8bcfbc36047eb15adbf48a3797464c8ba6a70503
Anuj Sinha <sinha.anuj30@gmail.com>
2024-07-24 16:26:48 -0700

    test: update test to compare with expected output from file.

--- tests/workflows/test_tsc_usd.py
+++ tests/workflows/test_tsc_usd.py
@@ -1,17 +1,14 @@
 import os
 from argparse import Namespace
-from tempfile import TemporaryDirectory
 
 import pandas as pd
 
 from mawpy.constants import (USER_ID, UNIX_START_T, UNIX_START_DATE, ORIG_LAT, ORIG_LONG, ORIG_UNC, STAY_LAT, STAY_LONG,
                              STAY, STAY_DUR)
-from mawpy.steps.trace_segmentation_clustering import trace_segmentation_clustering
-from mawpy.steps.update_stay_duration import update_stay_duration
-from mawpy.workflows.workflow2 import workflow2
+from mawpy.workflows.tsc_usd import tsc_usd
 
 
-def test_workflow1_tsc_usd(tmp_path):
+def test_tsc_usd(tmp_path):
 
     input_file = os.path.dirname(__file__) + '/../resources/test_input.csv'
     output_file = os.path.join(tmp_path, 'test_output_workflow2.csv')
@@ -25,22 +22,17 @@ def test_workflow1_tsc_usd(tmp_path):
     )
 
     # Run the workflow to be tested and get the output
-    actual_output_df = workflow2(args.input_file, args.output_file, args.spatial_constraint,
+    actual_output_df = tsc_usd(args.input_file, args.output_file, args.spatial_constraint,
                                  args.duration_constraint_1, args.duration_constraint_2)
 
     # Check if output file was created
     assert os.path.exists(output_file)
 
-    # Destroy test output file after test completed
-    os.remove(args.output_file)
-
-    # Check if the workflow steps were called with expected arguments
-    df_output = trace_segmentation_clustering(output_file, args.spatial_constraint, args.duration_constraint_1,
-                                              input_file=args.input_file)
-    expected_output_df = update_stay_duration(output_file, args.duration_constraint_2, input_df=df_output)
+    expected_output_df = pd.read_csv(os.path.dirname(__file__)
+                                     + '/../resources/tsc_usd_output_for_test_input.csv')
 
     # Assert if the expected_output_df equal to actual_output_df
-    pd.testing.assert_frame_equal(actual_output_df, expected_output_df)
+    pd.testing.assert_frame_equal(actual_output_df, expected_output_df, check_like=True)
 
     # List of columns expected in the actual_output_df
     expected_output_columns = [USER_ID, UNIX_START_T, UNIX_START_DATE,
