3109c9b9a86bdb88d641ffcf9e81d00da07564fc
Anuj Sinha <sinha.anuj30@gmail.com>
2024-07-24 16:26:10 -0700

    refactor: update workflow names

--- src/mawpy/workflows/ic_usd.py
+++ src/mawpy/workflows/ic_usd.py
@@ -24,7 +24,7 @@ parser.add_argument('--duration_constraint_2', default=300, type=float, required
 logger = logging.getLogger(__name__)
 
 
-def workflow1(
+def ic_usd(
     input_file: str,
     output_file: str,
     spatial_constraint: float,
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     multiprocessing.freeze_support() # TODO: do we require this? most probably NOT.
     st = datetime.datetime.now()
-    workflow1(args.input_file, "output_file.csv", args.spatial_constraint,
+    ic_usd(args.input_file, "output_file.csv", args.spatial_constraint,
               args.duration_constraint_1, args.duration_constraint_2)
     en = datetime.datetime.now()
     logger.info(f"Total Time taken for execution: {en - st}")
--- src/mawpy/workflows/tsc_usd.py
+++ src/mawpy/workflows/tsc_usd.py
@@ -22,7 +22,7 @@ parser.add_argument('--duration_constraint_2', default=300, type=float, required
 logger = logging.getLogger(__name__)
 
 
-def workflow2(
+def tsc_usd(
     input_file: str,
     output_file: str,
     spatial_constraint: float,
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     multiprocessing.freeze_support() # TODO: do we require this? most probably NOT.
     st = datetime.datetime.now()
-    workflow2(args.input_file, "output_file.csv", args.spatial_constraint,
+    tsc_usd(args.input_file, "output_file.csv", args.spatial_constraint,
               args.duration_constraint_1, args.duration_constraint_2)
     en = datetime.datetime.now()
     logger.info(f"Total Time taken for execution: {en - st}")
