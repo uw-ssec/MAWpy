47ce71e4f0b6b6e12cf536ceeb88ffb2ca8b3a2f
Anuj Sinha <sinha.anuj30@gmail.com>
2024-08-01 11:51:05 -0700

    fix: add type hints.

--- src/mawpy/utilities/validations.py
+++ src/mawpy/utilities/validations.py
@@ -1,12 +1,12 @@
 from mawpy.constants import INPUT_ARGS_LIST
 
 
-def check_non_negative(value):
+def check_non_negative(value: float) -> None:
     if value < 0:
         raise ValueError(f"{value} is not a valid non-negative numerical value")
 
 
-def validate_input_args(**kwargs):
+def validate_input_args(**kwargs: float) -> None:
     for k, val in kwargs.items():
         if k in INPUT_ARGS_LIST:
             check_non_negative(val)
